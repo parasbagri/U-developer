d21492053aa71626c01be2ea7744e3c61b3e36f9
Paras Bagri <parasba85@gmail.com>
2024-10-07 15:42:27 +0530

    E-Come

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+// import "./App.css";
+import "./categories.style.scss";
 
 function App() {
+  const cotegories = [
+    {
+      id: 1,
+      title: "Hates",
+      imageUrl:
+        "https://images.pexels.com/photos/28359348/pexels-photo-28359348/free-photo-of-a-woman-in-a-black-dress-stands-in-a-field.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      id: 2,
+      title: "Jakets",
+      imageUrl:
+        "https://images.pexels.com/photos/28359348/pexels-photo-28359348/free-photo-of-a-woman-in-a-black-dress-stands-in-a-field.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+      imageUrl:
+        "https://images.pexels.com/photos/28359348/pexels-photo-28359348/free-photo-of-a-woman-in-a-black-dress-stands-in-a-field.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      id: 4,
+      title: "Womens",
+      imageUrl:
+        "https://images.pexels.com/photos/25185199/pexels-photo-25185199/free-photo-of-smiling-woman-with-yellow-wildflowers-in-her-hair.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      id: 5,
+      title: "Mens",
+      imageUrl:
+        "https://images.pexels.com/photos/28359348/pexels-photo-28359348/free-photo-of-a-woman-in-a-black-dress-stands-in-a-field.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="categories-container">
+      {cotegories.map(({ title, id, imageUrl }) => (
+        <div key={id} className="category-container">
+          <div
+            className="background-image"
+            style={{
+              backgroundImage: `url(${imageUrl})`,
+            }}
+          />
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
