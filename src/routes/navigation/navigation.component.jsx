4bdffb4ab1665bb208355ceba6d6b2b160e4fe4a
Paras Bagri <parasba85@gmail.com>
2024-10-08 18:33:34 +0530

    Addind SCSS And SVG Inage Logo

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,12 +1,13 @@
 import { Fragment } from "react";
 import { Outlet, Link } from "react-router-dom";
-
+import {ReactComponent as CrwnLogo} from '../../assets/crown.svg'
+import './navigation.style.scss'
 const NaveBar = () => {
     return (
       <Fragment>
         <div className="navigation">
             <Link to='/'className="logo-container"> 
-            <div>LOGO</div>
+            <CrwnLogo className="logo"/>
             </Link>
             <div className="nav-links-container">
                 <Link to="/Shop" className="nav-link">Shop</Link>
